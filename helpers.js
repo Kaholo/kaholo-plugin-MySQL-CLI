c072e157f57e7511c2f97e04872c1efa90890c19
xkcm <xkcm16@gmail.com>
2022-12-19 12:56:25 +0100

    Add linting

--- helpers.js
+++ helpers.js
@@ -1,35 +1,47 @@
-const child_process = require("child_process");
-const {ConnectionString} = require('connection-string');
+const childProcess = require("child_process");
+const { ConnectionString } = require("connection-string");
 
-async function execCmd(command, args, description, path){
-    let opts = {};
-    if (path) opts.cwd = path;
-    return new Promise((resolve, reject) => {
-        child_process.execFile(command, args, opts, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`${description} error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`${description} stderr: ${stderr}`);
-            }
-            return resolve(stdout);
-        });
+async function execCmd(command, args, description, path) {
+  const opts = {};
+  if (path) {
+    opts.cwd = path;
+  }
+  return new Promise((resolve, reject) => {
+    childProcess.execFile(command, args, opts, (error, stdout, stderr) => {
+      if (error) {
+        return reject(new Error(`${description} error: ${error}`));
+      }
+      if (stderr) {
+        console.info(`${description} stderr: ${stderr}`);
+      }
+      return resolve(stdout);
     });
+  });
 }
 
-function conStrToArgs(conStr, dbNeeded){
-    let args = [];
-    const conObj = new ConnectionString(conStr);
-    if (conObj.hasOwnProperty("user"))              args.push("-u", conObj.user);
-    if (conObj.hasOwnProperty("password"))          args.push(`-p${conObj.password}`);
-    if (conObj.hostname)                            args.push("-h", conObj.hostname);   // conObj.hosts[0].name
-    if (conObj.port)                                args.push("-P", conObj.port);       // conObj.hosts[0].port
-    if (dbNeeded && conObj.hasOwnProperty("path"))  args.push("-D", conObj.path.join("/"));
-    
-    return args;
+function conStrToArgs(conStr, dbNeeded) {
+  const args = [];
+  const conObj = new ConnectionString(conStr);
+  if (Reflect.has(conObj, "user")) {
+    args.push("-u", conObj.user);
+  }
+  if (Reflect.has(conObj, "password")) {
+    args.push(`-p${conObj.password}`);
+  }
+  if (Reflect.has(conObj, "hostname")) {
+    args.push("-h", conObj.hostname);
+  }
+  if (Reflect.has(conObj, "port")) {
+    args.push("-P", conObj.port);
+  }
+  if (dbNeeded && Reflect.has(conObj, "path")) {
+    args.push("-D", conObj.path.join("/"));
+  }
+
+  return args;
 }
 
 module.exports = {
-    execCmd,
-    conStrToArgs
+  execCmd,
+  conStrToArgs,
 };
